8d510cc83860f467aeb9f618765a5b17db316d8f
Andrew Shmorhun <andrewshmorgun@gmail.com>
2022-11-15 17:05:06 +0200

    adopt registration page

--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -1,56 +1,143 @@
 import styled from "styled-components";
-import { FaTimes } from "react-icons/fa";
-import logo from "../logos/logo.png";
+import { GrFacebook } from "react-icons/gr";
+import { useState } from "react";
+
+// const InitialState = {
+//   name: "",
+//   password: "",
+//   email: "",
+//   isMember: false,
+// };
+
 export const Registration = ({
   setShowRegistration,
 }: {
   setShowRegistration: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
+  // const [values, setValues] = useState(InitialState);
+
+  const [login, setIsLogin] = useState(false);
+
+  // const handleChange = (e) => {
+  //   e.preventDefault();
+  //   const name = e.target.name;
+  //   const value = e.target.value;
+  //   setValues({ ...values, [name]: value });
+  // };
+
+  // const onSubmit = (e) => {
+  //   e.preventDefault();
+  //   const { name, email, password, isMember } = values;
+  //   if (!email || !password || (!isMember && !name)) {
+  //     toast.error("Please Fill Out All Fields");
+  //     return;
+  //   }
+  //   if (isMember) {
+  //     dispatch(loginUser({ email: email, password: password }));
+  //     return;
+  //   }
+  //   dispatch(registerUser({ email, password, name }));
+  // };
+
+  // const toggleMember = () => {
+  //   setValues({ ...values, isMember: !values.isMember });
+  // };
+
   return (
     <Wrapper>
       <div className="registration-container show-registration">
-        <div className="content">
+        <div className="content" style={{ height: login ? "80rem" : "65rem" }}>
           <header>
-            <img src={logo} alt="" className="logo" />
+            {/* <img src={logo} alt="Plovo logo" className="logo" /> */}
           </header>
-          <h1>Sign up to Plova</h1>
+          <h1>{login ? "Sign up to Plova" : "Login to Plova"}</h1>
           <button
             className="close-btn"
             // onClick={toggle}
           >
-            <FaTimes onClick={() => setShowRegistration(false)} />
+            <img
+              src="https://res.cloudinary.com/glovoapp/image/fetch//q_auto/https://glovoapp.com/images/close-icon.svg"
+              alt="close x"
+              onClick={() => setShowRegistration(false)}
+            />
           </button>
-          <button className="btn-fb btn center">Facebook</button>
-          <p className="center">or</p>
+          <button className="btn-fb btn center">
+            <div className="center">
+              <GrFacebook className="fb-icon" />
+              <p>Facebook</p>
+            </div>
+          </button>
+          <p className="center" style={{ fontSize: "2rem" }}>
+            or
+          </p>
           <form action="" className="form">
-            <div className="input-line">
-              <p style={{ paddingRight: "1rem" }}>🙂</p>
-              <input type="text" className="form-input" placeholder="name" />
+            {login && (
+              <div className="input-line center">
+                <div style={{ paddingRight: "1rem" }}>
+                  <img
+                    className="form-icon"
+                    src="https://res.cloudinary.com/glovoapp/image/fetch//q_auto/https://glovoapp.com/images/glyphs/person.svg"
+                    alt=""
+                  />
+                </div>
+                <input
+                  type="text"
+                  className="form-input"
+                  required
+                  placeholder="First name"
+                />
+              </div>
+            )}
+            <div className="input-line center">
+              <div style={{ paddingRight: "1rem" }}>
+                <img
+                  className="form-icon"
+                  src="https://res.cloudinary.com/glovoapp/image/fetch//q_auto/https://glovoapp.com/images/glyphs/letter.svg"
+                  alt=""
+                />
+              </div>
+              <input type="e-mail" className="form-input" placeholder="Email" />
             </div>
-            <div className="input-line">
-              <p style={{ paddingRight: "1rem" }}>📧</p>
-              <input
-                type="e-mail"
-                className="form-input"
-                placeholder="e-mail"
-              />
-            </div>
-            <div className="input-line">
-              <p style={{ paddingRight: "1rem" }}>🔐</p>
+            <div className="input-line center">
+              <div style={{ paddingRight: "1rem" }}>
+                <img
+                  className="form-icon"
+                  src="https://res.cloudinary.com/glovoapp/image/fetch//q_auto/https://glovoapp.com/images/glyphs/lock.svg"
+                  alt=""
+                />
+              </div>
               <input
                 type="password"
                 className="form-input"
-                placeholder="password"
+                placeholder="Password"
               />
             </div>
           </form>
-          <button className="btn-fb btn center">Sign up with e-mail</button>
+          <button
+            className={
+              login ? `btn-email btn center` : "btn-email_active btn center"
+            }
+          >
+            <p>Sign up with email</p>
+          </button>
           <p>
-            Get an account already? <a href="#">Log in</a>
+            {login ? "Get an account already?" : "New in Plova?"}
+            <button className="login-button" onClick={() => setIsLogin(!login)}>
+              {login ? "Log in" : "Sign up"}
+            </button>
           </p>
-          <div className="nav-links">
-            {/* <NavLinks toggleSidebar={toggle} /> */}
-          </div>
+          {login ? (
+            <div className="footer center">
+              By creating an account, you automatically accept our
+              <div>
+                <a href="#">Terms of service, </a>
+                <a href="#"> Privacy Policy</a> and
+                <a href="#"> Cookies Policy</a>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </Wrapper>
@@ -68,11 +155,15 @@ const Wrapper = styled.aside`
     background: rgba(0, 0, 0, 0.7);
     display: flex;
     justify-content: center;
-    align-items: center;
+    padding: 8.5rem 0;
     z-index: -1;
     opacity: 0;
     transition: var(--transition);
     overflow: auto;
+  }
+
+  .registration-container::-webkit-scrollbar {
+    width: 0;
   }
 
   .show-registration {
@@ -83,46 +174,132 @@ const Wrapper = styled.aside`
   .content {
     background: #fff;
     width: 60rem;
-    height: 75rem;
     border-radius: 1rem;
-    // margin-top: 30rem;
-    padding: 4rem 2rem;
+    padding: 1rem 2rem;
     position: relative;
     display: flex;
     align-items: center;
     flex-direction: column;
-    gap: 4rem;
+    gap: 3.8rem;
   }
 
   .close-btn {
     position: absolute;
-    top: 10px;
-    right: 10px;
+    top: 2.6rem;
+    right: 2.6rem;
     background: transparent;
     border-color: transparent;
-    font-size: 2rem;
     color: grey;
     cursor: pointer;
+  }
+
+  .btn-fb {
+    width: 33.8rem;
+    height: 4.8rem;
+    background-color: #3c539a;
+    color: white;
+    text-transform: uppercase;
+    padding: 0;
+    letter-spacing: 0.05rem;
+    font-size: 1.7rem;
+
+    p {
+      color: white;
+      font-weight: 600;
+      font-size: 1.7rem;
+    }
+  }
+
+  .btn-fb:hover {
+    background-color: #30427b;
+  }
+
+  .btn-email {
+    margin-top: 2rem;
+    width: 33.8rem;
+    height: 4.8rem;
+    color: white;
+    // text-transform: uppercase;
+    padding: 0;
+    letter-spacing: 0.05rem;
+    font-size: 1.7rem;
+    background: #c1c1c1;
+
+    p {
+      color: white;
+      font-weight: 600;
+      font-size: 1.8rem;
+    }
+  }
+  .btn-email:hover {
+    cursor: not-allowed;
+  }
+
+  .btn-email_active {
+    margin-top: 2rem;
+    width: 33.8rem;
+    height: 4.8rem;
+    background-color: #00a082ff;
+    color: white;
+    // text-transform: uppercase;
+    padding: 0;
+    letter-spacing: 0.05rem;
+    font-size: 1.7rem;
+
+    p {
+      color: white;
+      font-weight: 600;
+      font-size: 1.8rem;
+    }
+  }
+
+  .btn-email_active:hover {
+    background-color: #006653ff;
+  }
+
+  .fb-icon {
+    height: auto;
+    margin-right: 1rem;
+    margin-bottom: 0.1rem;
   }
 
   .form {
     display: flex;
     flex-direction: column;
+    width: 40rem;
     gap: 4rem;
-  }
-
-  .form-input {
-    border: none;
-    border-bottom: solid 1px grey;
+    color: #838383;
   }
 
   .input-line {
-    width: 26rem;
+    height: 5rem;
     display: flex;
+  }
+
+  .form-input {
+    height: 4rem;
+    width: 35rem;
+    border: none;
+    border-bottom: solid 2px grey;
+    font-size: 1.8rem;
   }
 
   .form-input:focus {
     outline: none;
+  }
+
+  .form-icon {
+    width: 2.6rem;
+  }
+
+  .login-button {
+    border: none;
+    background: none;
+    padding: 0rem 2rem;
+    color: #00a082ff;
+    font-weight: 700;
+    font-size: 1.6rem;
+    cursor: pointer;
   }
 
   .logo {
@@ -160,15 +337,28 @@ const Wrapper = styled.aside`
     transition: var(--transition);
   }
 
-  .active {
-    color: var(--grey-900);
+  .footer {
+    width: 40rem;
+    flex-direction: column;
   }
 
-  .icon {
-    color: var(--primary-500);
+  h1 {
+    font-size: 3rem;
+  }
+
+  a {
+    color: #838383;
+    text-decoration: none;
+    font-weight: 600;
+  }
+
+  a:hover {
+    text-decoration: underline;
   }
 
   p {
-    font-size: 2rem;
+    font-size: 1.6rem;
+    color: #838383;
+    font-weight: 400;
   }
 `;
